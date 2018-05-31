815e6789577061f66b2ec0cd96227c3def9ca17d
vinyasbj <vinyas555@gmail.com>
2018-06-01 00:29:26 +0530

    Open weather api Integration and api calls

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ import Form from "./component/Form";
 const API_KEY = "a2fbdc33f4b42915adf6e30e2cf6f9ac";
 
 class App extends React.Component {
+getWeather = async(e) => {
+  e.preventDefault();
+  const city = e.target.elements.city.value;
+  const country = e.target.elements.country.value;
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+  const data = await api_call.json();
+  console.log(data);
+}
   render(){
     return(
       <div>
         <Titles/>
-        <Form/>
+        <Form getWeather= {this.getWeather}/>
       </div>
     );
   }
