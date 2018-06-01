0fec28acfda660d0ea2b9446c84c2437de4fbed7
vinyasbj <vinyas555@gmail.com>
2018-06-01 20:52:35 +0530

    learnt about state and setstate

--- src/App.js
+++ src/App.js
@@ -2,27 +2,51 @@ import React from 'react';
 
 import Titles from "./component/Titles";
 import Form from "./component/Form";
-
+import Weather from "./component/Weather";
 
 const API_KEY = "a2fbdc33f4b42915adf6e30e2cf6f9ac";
 
 class App extends React.Component {
-getWeather = async(e) => {
-  e.preventDefault();
-  const city = e.target.elements.city.value;
-  const country = e.target.elements.country.value;
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
-  const data = await api_call.json();
-  console.log(data);
-}
+  state = {
+    temperature: undefined,
+    city: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async(e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    // console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
+  }
   render(){
     return(
       <div>
         <Titles/>
         <Form getWeather= {this.getWeather}/>
+        <Weather 
+          temperature={this.state.temperature}
+          city = {this.state.city}
+          country = {this.state.country}
+          humidity = {this.state.humidity}
+          description = {this.state.description}
+          error = {this.state.error}
+        />
       </div>
     );
   }
-}
+};
 
 export default App;
---
+++ src/component/Weather.js
@@ -0,0 +1,17 @@
+import React from 'react' ; 
+
+class Weather extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Location: {this.props.city},{this.props.country}</p>
+				<p>Temperature: {this.props.temperature}</p>
+				<p>Humidity: {this.props.humidity}</p>
+				<p>Conditions: {this.props.description}</p>
+			</div>
+		);
+	}
+}
+
+
+export default Weather;
