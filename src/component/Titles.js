4c1bef3885391ce77c9ce14f03ea3f93ec00e89f
vinyasbj <vinyas555@gmail.com>
2018-06-01 23:32:42 +0530

    rectified state less componet

--- src/component/Titles.js
+++ src/component/Titles.js
@@ -1,15 +1,11 @@
 import React from 'react' ; 
 
-class Titles extends React.Component {
-	render() {
-		return (
-			<div>
-			<h1>Weather finder</h1>
-			<p> Find out temperature ,conditions and more...</p>
-			</div>
-		);
-	}
-}
+const Titles = () => (
+<div>
+	<h1>Weather finder</h1>
+	<p> Find out temperature ,conditions and more...</p>
+</div>
+)
 
 
 export default Titles;
